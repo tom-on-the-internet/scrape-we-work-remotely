3415ea180d9bcf68cfff926d07837f0257f4aa05
tom <thomaspatricksteven@gmail.com>
2019-10-13 21:55:36 -0400

    Initialize repo

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Axios, { AxiosResponse } from 'axios';
+import cheerio from 'cheerio';
+import { writeFileSync } from 'fs';
+
+import {
+  baseUrl,
+  categoryUrl,
+  extractJobPostingUrlsFromJobListingHtml,
+} from './utilities/extract-job-posting-urls-from-job-listing-html';
+import extractLocationFromString from './utilities/extract-location-from-string';
+import extractTechnologiesFromString from './utilities/extract-technologies-from-string';
+
+const main = async () => {
+  const response: AxiosResponse = await Axios.get(baseUrl + categoryUrl);
+
+  const jobPostingUrls: string[] = extractJobPostingUrlsFromJobListingHtml(
+    response.data,
+  );
+
+  const responses = await Promise.all(
+    jobPostingUrls.map((url) => Axios.get(url)),
+  );
+
+  const postingPages = responses.map(
+    (postingsResponse: AxiosResponse) => postingsResponse.data,
+  );
+
+  const jobPostings = postingPages.map((postingPage) => {
+    const $ = cheerio.load(postingPage);
+
+    const location = extractLocationFromString(
+      $('.listing-tag')
+        .get()
+        .filter((_item, index) => index > 1)
+        .map((item) => $(item).text())[0],
+    );
+
+    const posting = $('#job-listing-show-container')!.html();
+    const technologies = extractTechnologiesFromString(posting);
+
+    return { location, technologies };
+  });
+
+  const jobPostingJSON = JSON.stringify(jobPostings);
+
+  writeFileSync('technologies.json', jobPostingJSON);
+};
+
+main();
---
+++ src/utilities/extract-job-posting-urls-from-job-listing-html.ts
@@ -0,0 +1,18 @@
+import cheerio from 'cheerio';
+
+export const baseUrl = 'https://weworkremotely.com';
+export const categoryUrl = '/categories/remote-programming-jobs/';
+
+export function extractJobPostingUrlsFromJobListingHtml(
+  jobListingHTML: any,
+): string[] {
+  const $ = cheerio.load(jobListingHTML);
+
+  return $('article li > a')
+    .get()
+    .map((item) => {
+      const relativeUrl = $(item).attr('href');
+      return baseUrl + relativeUrl;
+    })
+    .filter((url) => url !== `${baseUrl}/`);
+}
